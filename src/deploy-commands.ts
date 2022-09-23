afe251d2de14956bf80d586fb8a1fa4958ea2ebf
H01001000 <heiheiho000@gmail.com>
2022-09-23 13:24:44 +0800

    refactor: use v10 rest version

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
 
 import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import { Routes } from 'discord-api-types/v10';
 import commands from './commands';
 
 dotenv.config();
 
 if (!process.env.token || !process.env.clientId) process.exit(1);
 
-const rest = new REST({ version: '9' }).setToken(process.env.token);
+const rest = new REST({ version: '10' }).setToken(process.env.token);
 
 rest.put(
   Routes.applicationCommands(process.env.clientId),
