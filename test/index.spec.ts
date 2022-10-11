3f9e05708464df8ad6b2eac9b90919ec83059f16
H01001000 <heiheiho000@gmail.com>
2022-10-11 09:07:59 +0800

    refactor(test): preheat commands

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,46 +5,48 @@ import client from '../src/structure/client';
 import { genId } from './testingUtils';
 
 describe('index.ts', () => {
+  before(() => {
+    import('../src/commands');
+  });
+
+  beforeEach(() => {
+    // stub login for all test below since we tested
+    sinon.stub(client, 'login');
+  });
+
   afterEach(() => {
     client.removeAllListeners();
     sinon.restore();
     delete require.cache[require.resolve('../src/index')];
   });
 
-  describe('', () => {
-    it('will login discord with env variable "token"', () => {
-      const stubClientLogin = sinon.stub(client, 'login');
-      process.env.token = genId();
+  it('will login discord with env variable "token"', () => {
+    sinon.restore();
+    const stubClientLogin = sinon.stub(client, 'login');
+    process.env.token = genId();
 
-      require('../src/index');
+    require('../src/index');
 
-      // only login once
-      assert.isTrue(stubClientLogin.calledOnce);
-      // login with token
-      assert.deepEqual(stubClientLogin.args[0], [process.env.token]);
-    });
+    // only login once
+    assert.isTrue(stubClientLogin.calledOnce);
+    // login with token
+    assert.deepEqual(stubClientLogin.args[0], [process.env.token]);
   });
-  describe('', () => {
-    beforeEach(() => {
-      // stub login for all test below since we tested
-      sinon.stub(client, 'login');
-    });
 
-    it('will call dotenv config', () => {
-      const stubDotenvConfig = sinon.stub(dotenv, 'config');
-      require('../src/index');
-      assert.isTrue(stubDotenvConfig.calledOnce);
-    });
+  it('will call dotenv config', () => {
+    const stubDotenvConfig = sinon.stub(dotenv, 'config');
+    require('../src/index');
+    assert.isTrue(stubDotenvConfig.calledOnce);
+  });
 
-    it('will log ready when client ready', () => {
-      const stubConsoleLog = sinon.stub(console, 'log');
+  it('will log ready when client ready', () => {
+    const stubConsoleLog = sinon.stub(console, 'log');
 
-      require('../src/index');
-      client.emit('ready', client);
+    require('../src/index');
+    client.emit('ready', client);
 
-      // console log ready
-      assert.isTrue(stubConsoleLog.calledOnce);
-      assert.deepEqual(stubConsoleLog.args[0], ['ready']);
-    });
+    // console log ready
+    assert.isTrue(stubConsoleLog.calledOnce);
+    assert.deepEqual(stubConsoleLog.args[0], ['ready']);
   });
 });
