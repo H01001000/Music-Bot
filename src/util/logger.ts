6881fd810347d71edcc4c00b11065f4fd72ac66c
H01001000 <heiheiho000@gmail.com>
2022-10-13 14:27:10 +0800

    chore: show pure text for consoel log

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,11 +2,11 @@ import winston, { format } from 'winston';
 
 export default winston.createLogger({
   format: format.combine(
-    format.timestamp({ format: () => new Date().toLocaleString('hk') }),
+    format.timestamp({ format: () => new Date().toLocaleString() }),
     format.json(),
   ),
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({ format: format.simple() }),
     new winston.transports.File({ filename: 'combined.log' }),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
   ],
