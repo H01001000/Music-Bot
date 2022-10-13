db167b99e085831adb7f9256673f01b66b88afdb
H01001000 <heiheiho000@gmail.com>
2022-10-13 20:44:59 +0800

    refactor: rewrite then to await

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -13,8 +13,12 @@ if (!process.env.token || !process.env.clientId) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.token);
 
-rest.put(
-  Routes.applicationCommands(process.env.clientId),
-  { body: commands.map((command) => command.data.toJSON()) },
-).then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error);
+try {
+  await rest.put(
+    Routes.applicationCommands(process.env.clientId),
+    { body: commands.map((command) => command.data.toJSON()) },
+  );
+  console.log('Successfully registered application commands.');
+} catch (error) {
+  console.error(error);
+}
